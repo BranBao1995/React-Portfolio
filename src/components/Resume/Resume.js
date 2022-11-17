82540f9af321354c2fc774ccf482189588a14214
BranBao1995 <brandon.0702@hotmail.com>
2022-11-16 21:43:04 -0500

    added resume section

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,53 @@
 import React from "react";
+import { saveAs } from "file-saver";
+import { FaCheck } from "react-icons/fa";
+import resume from "../../resume.docx";
 import "./Resume.css";
 
 function Resume() {
-  return <div>Resume placeholder</div>;
+  const proficiencies = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "jQuery.js",
+    "React.js",
+    "Node.js",
+    "Express.js",
+    "MySQL",
+    "Sequalize.js",
+    "MongoDB",
+    "Mongoose",
+    "PWA Development",
+    "MERN Stack App Development",
+    "GraphQL",
+  ];
+
+  const downloadResume = () => {
+    saveAs(resume, "resume.docx");
+  };
+
+  return (
+    <div className="resume">
+      <h1 className="resume-title" onClick={downloadResume}>
+        Download My Resume
+      </h1>
+      <div className="proficiencies">
+        <h2 className="proficiencies-title">Check out my skillset</h2>
+        <ul className="proficiencies-list">
+          {proficiencies.map((proficiency) => {
+            return (
+              <li className="proficiencies-list-item">
+                <FaCheck />
+                <span className="proficiencies-list-item-name">
+                  {proficiency}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
